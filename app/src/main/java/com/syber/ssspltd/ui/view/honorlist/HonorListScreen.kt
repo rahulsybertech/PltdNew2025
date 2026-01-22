00ae7668ea6f2c8b610b54ce2a36148a7876190b
chandra <chandra@ssssyber.com>
2026-01-22 16:30:15 +0530

    remove some filed

--- app/src/main/java/com/syber/ssspltd/ui/view/honorlist/HonorListScreen.kt
+++ app/src/main/java/com/syber/ssspltd/ui/view/honorlist/HonorListScreen.kt
@@ -67,7 +67,7 @@ fun HonorListScreen(navController: NavController, viewModel1: AuthViewModel, the
 
 
         //    addProperty("PARTYCODE", AppSharedPreferences.getInstance(context).isPartyCode)
-            addProperty("FROMDATE", "")
+         //   addProperty("FROMDATE", "")
             addProperty("FROMDATE", "")
             addProperty("TODATE", "")
             addProperty("Status", "")
